b0a2b327da1519391f55422b818331fd783d2f3a
ramzeta <ramiroperezsanz13@gmail.com>
2022-12-16 17:35:28 +0100

    update

---
+++ txt.py
@@ -0,0 +1,7 @@
+
+with open("sample.txt", "a+") as file_object:
+    file_object.seek(0)
+    data = file_object.read(100)
+    if len(data) > 0 :
+        file_object.write("\n")
+    file_object.write("hello hi")
